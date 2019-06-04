b067061b3f9b81eb29d024f8e9fedd64c462cdeb
topepo <mxkuhn@gmail.com>
2019-06-04 16:09:58 -0400

    fixed outdated ggplot code

--- part-1-basics/examples/sidebar_app/server.R
+++ part-1-basics/examples/sidebar_app/server.R
@@ -4,10 +4,11 @@ data(mpg)
 
 shinyServer(
   function(input, output) {
+    
     output$that_plot <- renderPlot(
       ggplot(mpg, aes(x = displ, y = cty)) + 
         geom_point() + theme_bw() +
-        geom_smooth(method = loess, span = input$span)
+        geom_smooth(method = "loess", span = input$span)
     )
   }
 )
